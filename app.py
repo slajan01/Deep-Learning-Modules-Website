d3389546b7f2a113d02c8bb7a35785b6ffc09cb2
Honza Slavík <jan.slavik.ostrava@gmail.com>
2024-12-29 00:28:19 +0100

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def chatbot():
 
 # Main driver
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
